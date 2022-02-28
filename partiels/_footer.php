5e4031fc8c2db3ce98dfbea9a25047f4d8debda2
KernelBrain <60195610+KernelBrain@users.noreply.github.com>
2022-02-28 11:46:26 +0000

    Add files via upload

---
+++ partiels/_footer.php
@@ -0,0 +1,96 @@
+<footer class="main-footer">
+    <div class="footer-top" style="background: #04a038a9;"></div>
+    <div class="footer-main" style="margin-left:20%">
+        <div class="container">     
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-col">
+                        <h4 class="footer-title">A propos de nous <span class="title-under"></span></h4>
+                        <div class="footer-content">
+                            <p>
+                                Union Internationale pour la Fraternité et la Solidarité <strong>ONG-UIFS</strong> est une Organisation Non Gouvernementale à but non lucratif créée en Août 2011 en Côte d'Ivoire dans la ville de Bouaké par M. OUGA BI V. GASTON et de deux (2) Co-fondateurs dont un professeur de mathématiques du nom de M. KAMBIRÉ Antoine Marie et M. KOUAKOUA N'Guessan Eritier. Sa première vision lors de sa création était d'intervenir dans le domaine éducatif.
+                            </p> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-col">
+                        <h4 class="footer-title">LAST TWEETS <span class="title-under"></span></h4>
+                        <div class="footer-content">
+                            <ul class="tweets list-unstyled">
+                                <li class="tweet"> 
+                                    <i class="fa fa-phone"></i> <a href="tel:">(+225) 07-07-09-35-01/ 01-02-16-17-49/ 27-31-60-04-93 </a>
+                                </li>
+                                <li class="tweet"> 
+                                    <i class="fa fa-envelope"></i> <a href="mailto:onguefs.ci@gmail.com">onguefs.ci@gmail.com</a> 
+                                </li>
+                                <li class="tweet"> 
+                                    <i class="fa fa-location"></i> <a href="#">Bouaké Kennedy
+                                    Non loin du Collège la Maison de BAMBI (Côte d'Ivoire)</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+                <!--<div class="col-md-4">
+                    <div class="footer-col">
+                        <h4 class="footer-title">Contact us <span class="title-under"></span></h4>
+                        <div class="footer-content">
+                            <div class="footer-form">
+                                <div class="footer-form" >
+                                    <form action="php/mail.php" class="ajax-form">
+                                        <div class="form-group">
+                                            <input type="text" name="name" class="form-control" placeholder="Name" required>
+                                        </div>  
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <textarea name="message" class="form-control" placeholder="Message" required></textarea>
+                                        </div>
+                                        <div class="form-group alerts">
+                                            <div class="alert alert-success" role="alert"></div>
+                                            <div class="alert alert-danger" role="alert"></div>
+                                        </div>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-submit pull-right">Send message</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>-->
+                <div class="clearfix"></div>
+            </div>       
+        </div> 
+    </div>
+    <div class="footer-bottom" style="background: #04a038a9;">
+        <div class="container text-center">
+            ONG-UIFS @ copyrights 2022 - by <a href="http://www.ong-uifs.com" target="_blank">i-yelen(Kernel_MK)</a>
+        </div>
+    </div>
+    </footer> <!-- main-footer -->
+       <!-- /.modal -->
+        <!--  Scripts ================================================== -->
+        <!-- jQuery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.1.min.js"><\/script>')</script>
+        <!-- Bootsrap javascript file -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- owl carouseljavascript file -->
+        <script src="assets/js/owl.carousel.min.js"></script>
+        <!-- Template main javascript -->
+        <script src="assets/js/main.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                e.src='//www.google-analytics.com/analytics.js';
+                r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+                ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
---
+++ partiels/_nav.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title> ACCUEIL | ONG-UIFS /l'espoir  renaît pour un avenir meilleur</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/images/logo.jpeg" height="45px" width="50px">
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Dosis:400,700' rel='stylesheet' type='text/css'>
+
+        <!-- Bootsrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <!-- Owl carousel -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+
+        <!-- Template main Css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <!-- Modernizr -->
+        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <header class="main-header">    
+            <nav class="navbar navbar-static-top">
+                <div class="navbar-top"  style="background: #04a038a9;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-xs-12">
+                                <ul class="list-unstyled list-inline header-contact">
+                                    <li> <i class="fa fa-phone"></i> <a href="tel:">(+225) 07-07-09-35-01 / 01-02-16-17-49 / 27-31-60-04-93 </a> </li>
+                                    <li> <i class="fa fa-envelope"></i> <a href="mailto:onguefs.ci@gmail.com">onguefs.ci@gmail.com</a> </li>
+                            </ul> <!-- /.header-contact  -->  
+                            </div>
+                            <div class="col-sm-6 col-xs-12 text-right">
+                                <ul class="list-unstyled list-inline header-social">
+                                    <li> <a href="https://www.facebook.com/Ong.uefs.Internatinale"> <i class="fa fa-facebook"></i> </a> </li>
+                                <li> <a href="https://youtube.com/channel/UCTIJ-R24rKYglzU7W0oSrPw"> <i class="fa fa-youtube"></i>  </a> </li>
+                                    
+                            </ul> <!-- /.header-social  -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        <div class="navbar-main" style="background: #f7b604;">          
+            <div class="container">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="index.html"><img src="assets/images/logo.jpeg" height="45px" width="50px" alt=""></a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse pull-right">
+                    <ul class="nav navbar-nav">
+                        <li><a class="is-active" href="index.php">ACCUEIL</a></li>
+                        <li><a href="about.php">A PROPOS</a></li>
+                        <li class="has-child"><a href="#">NOS OEUVRES</a>
+                            <ul class="submenu">
+                                <li class="submenu-item"><a href="ecole.php" style="text-transform:uppercase">école </a></li>
+                                <li class="submenu-item"><a href="hopital.php" style="text-transform:uppercase">hopital </a></li>
+                                <li class="submenu-item"><a href="orphelinat.php" style="text-transform:uppercase">orphélinat </a></li>
+                               <!-- <li class="submenu-item"><a href="veuve.php" style="text-transform:uppercase">veuve </a></li>-->
+                            </ul>
+                        </li>
+                        <li><a href="galerie.php">GALERIE</a></li>
+                        <li><a href="contact.php">CONTACTEZ</a></li>
+                    </ul>
+                </div> <!-- /#navbar -->
+            </div> <!-- /.container -->  
+        </div> <!-- /.navbar-main -->
+    </nav> 
+</header> <!-- /. main-header -->
